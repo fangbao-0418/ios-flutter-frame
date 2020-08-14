af77ba65bd1bb4ebca540aec4054f84a90cf3008
gaoyanru <gaoyanru@mail.i-i.ai>
2020-08-15 02:09:02 +0800

    Initial Commit

---
+++ ios-flutter-frame/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ios-flutter-frame
+//
+//  Created by 宝宝 on 2020/8/15.
+//  Copyright © 2020 宝宝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
